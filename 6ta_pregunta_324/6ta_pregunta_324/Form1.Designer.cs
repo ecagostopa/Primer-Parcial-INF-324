aa248d2850a2aa1dc692e132cb9923e9b0a9c8e6
ecagostopa <147269219+ecagostopa@users.noreply.github.com>
2023-10-08 12:43:56 -0400

    Add files via upload

---
+++ 6ta_pregunta_324/6ta_pregunta_324/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace _6ta_pregunta_324
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textPaterno = new System.Windows.Forms.TextBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonModificar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.textGenero = new System.Windows.Forms.TextBox();
+            this.textLocacion = new System.Windows.Forms.TextBox();
+            this.textSemestre = new System.Windows.Forms.TextBox();
+            this.textMaterno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(128, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(662, 294);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(650, 272);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(22, 50);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(100, 20);
+            this.textUsuario.TabIndex = 1;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(22, 92);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(100, 20);
+            this.textPassword.TabIndex = 2;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(22, 131);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(100, 20);
+            this.textNombre.TabIndex = 3;
+            // 
+            // textPaterno
+            // 
+            this.textPaterno.Location = new System.Drawing.Point(22, 170);
+            this.textPaterno.Name = "textPaterno";
+            this.textPaterno.Size = new System.Drawing.Size(100, 20);
+            this.textPaterno.TabIndex = 4;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(214, 334);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(111, 23);
+            this.buttonInsert.TabIndex = 5;
+            this.buttonInsert.Text = "INSERTAR";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonModificar
+            // 
+            this.buttonModificar.Location = new System.Drawing.Point(372, 334);
+            this.buttonModificar.Name = "buttonModificar";
+            this.buttonModificar.Size = new System.Drawing.Size(111, 23);
+            this.buttonModificar.TabIndex = 6;
+            this.buttonModificar.Text = "MODIFICAR";
+            this.buttonModificar.UseVisualStyleBackColor = true;
+            this.buttonModificar.Click += new System.EventHandler(this.buttonModificar_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(518, 334);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(111, 23);
+            this.buttonEliminar.TabIndex = 7;
+            this.buttonEliminar.Text = "ELIMINAR";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // textGenero
+            // 
+            this.textGenero.Location = new System.Drawing.Point(22, 326);
+            this.textGenero.Name = "textGenero";
+            this.textGenero.Size = new System.Drawing.Size(100, 20);
+            this.textGenero.TabIndex = 12;
+            // 
+            // textLocacion
+            // 
+            this.textLocacion.Location = new System.Drawing.Point(22, 287);
+            this.textLocacion.Name = "textLocacion";
+            this.textLocacion.Size = new System.Drawing.Size(100, 20);
+            this.textLocacion.TabIndex = 11;
+            // 
+            // textSemestre
+            // 
+            this.textSemestre.Location = new System.Drawing.Point(22, 248);
+            this.textSemestre.Name = "textSemestre";
+            this.textSemestre.Size = new System.Drawing.Size(100, 20);
+            this.textSemestre.TabIndex = 10;
+            // 
+            // textMaterno
+            // 
+            this.textMaterno.Location = new System.Drawing.Point(22, 209);
+            this.textMaterno.Name = "textMaterno";
+            this.textMaterno.Size = new System.Drawing.Size(100, 20);
+            this.textMaterno.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Usuario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Locacion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Semestre";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Ap_Materno";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Ap_Paterno";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 379);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textGenero);
+            this.Controls.Add(this.textLocacion);
+            this.Controls.Add(this.textSemestre);
+            this.Controls.Add(this.textMaterno);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonModificar);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.textPaterno);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textPaterno;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonModificar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.TextBox textGenero;
+        private System.Windows.Forms.TextBox textLocacion;
+        private System.Windows.Forms.TextBox textSemestre;
+        private System.Windows.Forms.TextBox textMaterno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 6ta_pregunta_324/6ta_pregunta_324/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _6ta_pregunta_324
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string conexion = "SERVER=127.0.0.1;PORT=3306;DATABASE=mibd_agostopa;UID=root;PASSWORD=;";
+        MySqlConnection cn = new MySqlConnection(conexion);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        public DataTable llenar_grid()
+        {
+            cn.Open();
+            DataTable dt = new DataTable();
+            string llenar = "select * from estudiante";
+            MySqlCommand cmd = new MySqlCommand(llenar,cn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+        }
+        private void limpiar()
+        {
+            textUsuario.Clear();
+            textPassword.Clear();
+            textNombre.Clear();
+            textPaterno.Clear();
+            textMaterno.Clear();
+            textSemestre.Clear();
+            textLocacion.Clear();
+            textGenero.Clear();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            string insertar = "INSERT INTO estudiante (usuario, password, nombre, ap_paterno, ap_materno, semestre, locacion, genero) " +
+                "VALUES (@usuario, @password, @nombre, @ap_paterno, @ap_materno, @semestre, @locacion, @genero)";
+            MySqlCommand cmd = new MySqlCommand(insertar,cn);
+            cmd.Parameters.AddWithValue("@usuario",textUsuario.Text);
+            cmd.Parameters.AddWithValue("@password", textPassword.Text);
+            cmd.Parameters.AddWithValue("@nombre", textNombre.Text);
+            cmd.Parameters.AddWithValue("@ap_paterno", textPaterno.Text);
+            cmd.Parameters.AddWithValue("@ap_materno", textMaterno.Text);
+            cmd.Parameters.AddWithValue("@semestre", textSemestre.Text);
+            cmd.Parameters.AddWithValue("@locacion", textLocacion.Text);
+            cmd.Parameters.AddWithValue("@genero", textGenero.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+
+            MessageBox.Show("Datos AGREGADOS con exito");
+
+            dataGridView1.DataSource = llenar_grid();
+
+            limpiar();
+        }
+
+        private void buttonModificar_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            string actualizar = "UPDATE estudiante SET usuario=@usuario, password=@password," +
+                "nombre=@nombre, ap_paterno=@ap_paterno, ap_materno=@ap_materno, semestre=@semestre," +
+                "locacion=@locacion, genero=@genero WHERE usuario=@usuario";
+            MySqlCommand cmd = new MySqlCommand(actualizar,cn);
+            cmd.Parameters.AddWithValue("@usuario", textUsuario.Text);
+            cmd.Parameters.AddWithValue("@password", textPassword.Text);
+            cmd.Parameters.AddWithValue("@nombre", textNombre.Text);
+            cmd.Parameters.AddWithValue("@ap_paterno", textPaterno.Text);
+            cmd.Parameters.AddWithValue("@ap_materno", textMaterno.Text);
+            cmd.Parameters.AddWithValue("@semestre", textSemestre.Text);
+            cmd.Parameters.AddWithValue("@locacion", textLocacion.Text);
+            cmd.Parameters.AddWithValue("@genero", textGenero.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+
+            MessageBox.Show("Datos ACTUALIZADOS con exito");
+
+            dataGridView1.DataSource = llenar_grid();
+
+            limpiar();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                textUsuario.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                textPassword.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textNombre.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                textPaterno.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                textMaterno.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                textSemestre.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+                textLocacion.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+                textGenero.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            }
+            catch { }
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            string eliminar = "DELETE FROM estudiante WHERE usuario=@usuario";
+            MySqlCommand cmd = new MySqlCommand(eliminar,cn);
+            cmd.Parameters.AddWithValue("@usuario", textUsuario.Text);
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+
+            MessageBox.Show("Datos ELIMINADOS con exito");
+
+            dataGridView1.DataSource = llenar_grid();
+
+            limpiar();
+        }
+    }
+}
